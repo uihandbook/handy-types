d816ef81ded487e31b70da2e1fb4f2c5fbae8e87
Markie Arnold <markiearnold@gmail.com>
2020-06-06 22:32:01 -0500

    .

--- with-margin.ts
+++ with-margin.ts
@@ -6,25 +6,3 @@ export interface WithMargin {
     left: number
   } | number;
 };
-
-export const getMargin = (margin: WithMargin['margin']) => {
-  if(!margin) {
-    return null;
-  }
-
-  if(typeof margin === 'number') {
-    return `margin: ${margin}px;`;
-  } else if(typeof margin === 'object') {
-    const marginTop = margin['top'] ? margin['top'] : 0;
-    const marginRight = margin['right'] ? margin['right'] : 0;
-    const marginBottom = margin['bottom'] ? margin['bottom'] : 0;
-    const marginLeft = margin['left'] ? margin['left'] : 0;
-
-    return `margin-top: ${marginTop};
-      margin-right: ${marginRight};
-      margin-bottom: ${marginBottom};
-      margin-left: ${marginLeft};`;
-  } else {
-    return null;
-  }
-};
--- with-padding.ts
+++ with-padding.ts
@@ -6,25 +6,3 @@ export interface WithPadding {
     left: number
   } | number;
 };
-
-export const getPadding = (padding: WithPadding['padding']) => {
-  if(!padding) {
-    return null;
-  }
-
-  if(typeof padding === 'number') {
-    return `padding: ${padding}px;`;
-  } else if(typeof padding === 'object') {
-    const paddingTop = padding['top'] ? padding['top'] : 0;
-    const paddingRight = padding['right'] ? padding['right'] : 0;
-    const paddingBottom = padding['bottom'] ? padding['bottom'] : 0;
-    const paddingLeft = padding['left'] ? padding['left'] : 0;
-
-    return `padding-top: ${paddingTop};
-      padding-right: ${paddingRight};
-      padding-bottom: ${paddingBottom};
-      padding-left: ${paddingLeft};`;
-  } else {
-    return null;
-  }
-};
