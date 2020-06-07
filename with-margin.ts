1d8ec673e25585338213b16e16f7dc12824e7abd
Markie Arnold <markiearnold@gmail.com>
2020-06-07 12:16:47 -0500

    .

--- with-margin.ts
+++ with-margin.ts
@@ -1,8 +1,8 @@
 export interface WithMargin {
   margin?: {
-    top: string,
-    right: string,
-    bottom: string,
-    left: string
+    top?: string,
+    right?: string,
+    bottom?: string,
+    left?: string
   } | string;
 };
--- with-padding.ts
+++ with-padding.ts
@@ -1,8 +1,8 @@
 export interface WithPadding {
   padding?: {
-    top: string,
-    right: string,
-    bottom: string,
-    left: string
+    top?: string,
+    right?: string,
+    bottom?: string,
+    left?: string
   } | string;
 };
